440b60b5029d70ca71790f6d47aad3ff9215dd18
Artyom <volkovartik@gmail.com>
2025-01-16 15:41:36 +0300

    update everything to sdk 0.2.1.0

--- tests/payload/test_plugin_run.py
+++ tests/payload/test_plugin_run.py
@@ -13,7 +13,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.ie.webdriver import WebDriver
 
 from tests.config.fixtures import fix_plugin_config, project_config
-from tests.payload.fixtures import execute_timeout
+# from tests.payload.fixtures import execute_timeout
 from s3p_sdk.types import S3PRefer, S3PDocument, S3PPlugin, S3PPluginRestrictions
 from s3p_sdk.plugin.types import SOURCE
 
@@ -63,11 +63,11 @@ class TestPayloadRun:
     def run_payload(self, payload: Type[S3PParserBase], _plugin: S3PPlugin, driver: WebDriver, refer: S3PRefer, max_document: int,
                     timeout: int = 2):
         # !WARNING Требуется изменить путь до актуального парсера плагина
-        from src.s3_platform_plugin_template.template_payload import MyTemplateParser
-        if isinstance(payload, type(MyTemplateParser)):
+        from src.s3p_plugin_parser_eucommission.eucommission import EUCommission
+        if isinstance(payload, type(EUCommission)):
             _payload = payload(refer=refer, plugin=_plugin, restrictions=S3PPluginRestrictions(max_document, None, None, None), web_driver=driver)
 
-            @execute_timeout(timeout)
+            # @execute_timeout(timeout)
             def execute() -> tuple[S3PDocument, ...]:
                 return _payload.content()
 
